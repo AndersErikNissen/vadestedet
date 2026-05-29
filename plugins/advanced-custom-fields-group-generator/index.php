4245b59cec34015d43b28054a8b85ecca93f1a9c
Anders Erik Nissen <anderseriknissen@gmail.com>
2026-05-29 12:39:03 +0200

    removed carousel section

--- plugins/advanced-custom-fields-group-generator/index.php
+++ plugins/advanced-custom-fields-group-generator/index.php
@@ -298,69 +298,6 @@ function acfgg_block( $relation, $type ):array {
     ];
   };
 
-  if ( $type === 'carousel' ) {
-    $return_array = [];
-
-    $return_array[] = acfgg_accordion( $block_relation . 'tab_', 'Carousel' );
-    
-    $sub_field_relation = $block_relation . 'sub_field_';
-    $sub_fields = [];
-
-    $previous_keys = [];
-
-    for ( $i = 1; $i <= 8; $i++ ) {
-      $sub_field_relation_extention = $sub_field_relation . $i . '_';
-      
-      $heading       = acfgg_field( $sub_field_relation_extention, 'Overskrift',              'heading',       'text'                                        );
-      $description   = acfgg_field( $sub_field_relation_extention, 'Tekst',                   'text',          'textarea'                                    );
-      $button        = acfgg_field( $sub_field_relation_extention, 'Knap',                    'button',        'link'                                        );
-      $image_desktop = acfgg_field( $sub_field_relation_extention, 'Vælg billede (Computer)', 'image_desktop', 'image', [ 'wrapper' => [ 'width' => '50' ] ] );
-      $image_mobile  = acfgg_field( $sub_field_relation_extention, 'Vælg billede (Mobile)',   'image_mobile',  'image', [ 'wrapper' => [ 'width' => '50' ] ] );
-
-      $keys = [
-        $heading[ 'key' ],
-        $description[ 'key' ],
-        $button[ 'key' ],
-        $image_desktop[ 'key' ],
-        $image_mobile[ 'key' ],
-      ];
-
-      $conditional_logic = [];
-
-      if ( $i !== 1 ) {
-        $logic_keys = array_merge( $keys, $previous_keys );
-
-        foreach ( $logic_keys as $key ) {
-          $conditional_logic[] = [
-            [
-              'field' => $key,
-              'operator' => '!=empty'
-            ]
-          ];
-        }
-      }
-
-      $tab = acfgg_accordion( $sub_field_relation_extention . 'tab_', '(' . $i . ') Element', [
-        'conditional_logic' => $conditional_logic
-      ] );
-
-      $sub_fields[] = $tab;
-      $sub_fields[] = $heading;
-      $sub_fields[] = $description;
-      $sub_fields[] = $button;
-      $sub_fields[] = $image_desktop;
-      $sub_fields[] = $image_mobile;
-
-      $previous_keys = $keys;
-    };
-
-    $return_array[] = acfgg_field( $block_relation, 'Carousel elementer', 'elements', 'group', [
-      'sub_fields' => $sub_fields,
-    ] );
-
-    return $return_array;
-  };
-
   if ( $type === 'faq' ) {
     $return_array = [];
 
@@ -671,22 +608,6 @@ function acfgg_sections() {
       acfgg_location( [ 'page', 'not_frontpage' ] )
     ],
     5
-  );
-
-
-  // ## carousel
-  $relation = 'section_carousel_';
-
-  acfgg_group( 
-    $relation, 
-    'Sektion: Carousel', 
-    'carousel',
-    array_merge(
-      acfgg_block( $relation, 'carousel'  )
-    ), [
-      acfgg_location( [ 'frontpage' ] )
-    ],
-    1
   );
 
 
